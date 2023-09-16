fb0c54f130550b4f1f457ef19f1aa083059a4a70
Vadym Bei <bey1705@gmail.com>
2023-09-16 12:53:21 +0300

    EntityNotFoundException was updated

--- src/Services/Entities/Exceptions/EntityNotFoundException.cs
+++ src/Services/Entities/Exceptions/EntityNotFoundException.cs
@@ -7,18 +7,18 @@ namespace Entities.Exceptions
     public class EntityNotFoundException : CustomException
     {
         public EntityNotFoundException()
-            : base(HttpStatusCode.NotFound, "Not found exception", BaseErrorType.NotFoundError)
+            : base(HttpStatusCode.NotFound, "Not found exception", BaseErrorType.EntityNotFoundError)
         {
 
         }
 
         public EntityNotFoundException(string message)
-            : base(HttpStatusCode.NotFound, message, BaseErrorType.NotFoundError)
+            : base(HttpStatusCode.NotFound, message, BaseErrorType.EntityNotFoundError)
         {
         }
 
         public EntityNotFoundException(Type type, object entityId)
-            : base(HttpStatusCode.NotFound, $"Entity '{type.Name}' was not found with Id: {entityId}", BaseErrorType.NotFoundError)
+            : base(HttpStatusCode.NotFound, $"Entity '{type.Name}' was not found with Id: {entityId}", BaseErrorType.EntityNotFoundError)
         {
         }
     }
